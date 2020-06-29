2a2acf2a774c39c20894cd9241bf3e16b47d3f9b
hehengcp <425269483@qq.com>
2020-06-29 11:08:05 +0800

    no message

--- app/src/main/java/com/chinasofinc/zw/MyCertificateActivity.kt
+++ app/src/main/java/com/chinasofinc/zw/MyCertificateActivity.kt
@@ -2,6 +2,7 @@ package com.chinasofinc.zw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 
 class MyCertificateActivity : AppCompatActivity() {
@@ -9,5 +10,8 @@ class MyCertificateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_certificate)
         findViewById<TextView>(R.id.tv_title).text = "我的证书"
+        findViewById<View>(R.id.img_back).setOnClickListener {
+            onBackPressed()
+        }
     }
 }
